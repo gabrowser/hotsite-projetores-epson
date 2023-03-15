b2776946ff45b96ba127dfa40068c12d07e26d9a
Rodrigo Pedro <rodrigo.v.pedro@outlook.com>
2023-03-15 12:27:43 -0300

    Aplicação e Configuração da Galeria

--- scripts/splides.js
+++ scripts/splides.js
@@ -55,5 +55,34 @@ window.addEventListener('load', () => {
             }
         });
     }
-
-});
+    // Splide Carrossel
+    var main = new Splide( '#main-slider', {
+      type      : 'fade',
+      rewind    : true,
+      pagination: false,
+      arrows    : true,
+    } );
+  
+  
+    var thumbnails = new Splide( '#thumbnail-slider', {
+      fixedWidth  : 100,
+      fixedHeight : 60,
+      gap         : 10,
+      rewind      : true,
+      pagination  : false,
+      cover       : true,
+      isNavigation: true,
+      breakpoints : {
+        600: {
+          fixedWidth : 60,
+          fixedHeight: 44,
+        },
+      },
+    } );
+  
+  
+    main.sync( thumbnails );
+    main.mount();
+    thumbnails.mount();
+////////////
+} );
